baeb9d9ad6c3ebfb9fac5aad0e1a957dce83b9e7
Matias Mass <matiasmass14@gmail.com>
2022-04-04 13:38:07 -0300

    Fixing button

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { AdviceGenerator } from './components/AdviceGenerator';
 
 function App() {
   const [advice, setAdvices] = useState({})
   const [loading, setLoading] = useState(false)
-  useEffect(() => {
+
+
+  const handleCallAPI = () => {
     setLoading(true)
-      fetch("https://api.adviceslip.com/advice")
+    fetch("https://api.adviceslip.com/advice%22)
       .then((res) => res.json())
-      .then((json) => {console.log(json)
-      
-      let advice = {
-        id: json.slip.id,
-        advice: json.slip.advice
+      .then((json) => {
+        let advice = {
+          id: json.slip.id,
+          advice: json.slip.advice
         }
         setAdvices(advice)
         setLoading(false)
       })
-
-  }, [])
-  
-  
-  return (
-    <div className="App"> 
-    {
-      !loading ? <AdviceGenerator  id = {advice.id} advice = {advice.advice} /> : <h2 style = {{color: "white", margin: "100px"}}>Loading...</h2>
     }
-    </div>
-  );
-}
-
-export default App;
--- src/components/AdviceGenerator.js
+++ src/components/AdviceGenerator.js
@@ -3,18 +3,19 @@ import "./AdviceGenerator.css";
 import iconDice from "../images/icon-dice.svg";
 import divider from "../images/pattern-divider-desktop.svg";
 
-export const AdviceGenerator = ({ id, advice }) => {
+export const AdviceGenerator = ({ id, advice, handleCallAPI }) => {
   return (
+
     <>
+     {advice &&
       <div className="container">
         <h4 className="title">Advice #{id}</h4>
         <p>{advice}</p>
         <img className="divider" src={divider} alt="" />
-      </div>
-      <div className="dice" onClick={() => alert("cambiado")}>
+      </div>}
+      <div className="dice" onClick={handleCallAPI}>
         <img src={iconDice} alt="" />
       </div>
-
     </>
   );
-};
+}; 
