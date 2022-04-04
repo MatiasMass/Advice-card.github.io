f0c7f9270657423036cd07b7f8c979b7aeefbaa7
Matias Mass <matiasmass14@gmail.com>
2022-04-04 15:00:11 -0300

    fixing button

--- src/components/AdviceGenerator.js
+++ src/components/AdviceGenerator.js
@@ -5,17 +5,17 @@ import divider from "../images/pattern-divider-desktop.svg";
 
 export const AdviceGenerator = ({ id, advice, handleCallAPI }) => {
   return (
-
     <>
-     {advice &&
-      <div className="container">
+
+        <div className="container">
         <h4 className="title">Advice #{id}</h4>
         <p>{advice}</p>
         <img className="divider" src={divider} alt="" />
-      </div>}
+      </div>
       <div className="dice" onClick={handleCallAPI}>
         <img src={iconDice} alt="" />
       </div>
+    
     </>
   );
 }; 
